49c67ef23890d6f04aef6339e1b5ea44748982f5
Orangels <orangels0313@163.com>
2020-11-08 15:50:12 +0800

    Init

---
+++ src/common/urls.js
@@ -0,0 +1,21 @@
+// export let url = window.location.origin
+
+export let url = 'http://192.168.88.84:5000'
+
+// export let url = 'http://127.0.0.1:5000'
+// export let url = 'http://172.16.104.249:5000'
+// export let url = 'http://192.168.88.105:5000'
+// export let hostname = '192.168.88.105'
+
+export let hostname = window.location.hostname
+
+// export let hostname = 'http://192.168.88.84:5000'
+
+export let GET_RTMP_URL = `${url}/get_rtmp_url`
+export let ADD_RTMP_URL = `${url}/add_rtmp_url`
+export let DEL_RTMP_URL = `${url}/del_rtmp_url`
+export let GET_ALL_PERSONS = `${url}/all_persons`
+export let UPLOAD_IMG_TMP = `${url}/upload_tmp`
+export let ADD_PERSON = `${url}/add_person`
+export let change_remote_config = `${url}/change_remote_config`
+export let downloadfile = `${url}/downloadfile`
---
+++ src/view/Store/appStore.js
@@ -0,0 +1,59 @@
+import {observable, action} from 'mobx'
+import Queue_len from "../../common/dataStructs"
+
+import {deepCopy} from "../../common/utils";
+
+class AppStore {
+
+    @observable isLogin = true
+
+    @observable entryPersons = new Queue_len(8)
+
+    @observable imgageIcomCoors = []
+    // @observable heatMapPoints = {}
+    @observable heatMapPoints = {}
+    @observable trackIDsArr = {}
+    @observable trackerArr = []
+
+    @observable trackerTimestamp = "1970-01-01 00:00:00"
+
+    @observable entruUser = []
+
+
+    @action updateEntryPersons(vPersons){
+        // this.entryPersons = deepCopy(vPersons)
+        this.entryPersons = vPersons
+    }
+
+    @action updateImgageIcomCoors(vImgageIcomCoors){
+        this.imgageIcomCoors = deepCopy(vImgageIcomCoors)
+    }
+
+    @action updateHeatMapPoints(vHeatMapPoints) {
+        this.heatMapPoints = deepCopy(vHeatMapPoints)
+    }
+
+    @action updateTrackIDsArr(vTrackIDsArr) {
+        this.trackIDsArr = deepCopy(vTrackIDsArr)
+    }
+
+    @action updateTrackerArr(vTrackerArr){
+        this.trackerArr = deepCopy(vTrackerArr)
+    }
+
+    @action updateEntruUser(vEntruUser) {
+        // this.entruUser = deepCopy(vEntruUser)
+        this.entruUser = vEntruUser
+    }
+
+    @action updateTrackerTimestamp(vTimestamp){
+        this.trackerTimestamp = vTimestamp
+    }
+
+    @action initUsers() {
+        const localUsers = localStorage['users']?JSON.parse(localStorage['users']):[]
+        this.users = [{username: 'admin', password: 'admin', auth:0},...localUsers, ...this.users]
+    }
+}
+
+export default new AppStore()
